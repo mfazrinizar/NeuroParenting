3bc979fa1d47e0f19c78e0908e56c8f56ac7140b
M. Fazri Nizar <anon6372098.id@gmail.com>
2024-02-06 14:59:25 +0700

    Add invalid-credential error code handling.

--- lib/src/db/auth/login_api.dart
+++ lib/src/db/auth/login_api.dart
@@ -40,7 +40,8 @@ class LoginApi {
       // Return the error code
       if (e.code == 'wrong-password' ||
           e.code == 'invalid-email' ||
-          e.code == 'user-not-found') {
+          e.code == 'user-not-found' || 
+          e.code == 'invalid-credential') {
         return {'status': 'error', 'message': 'Wrong email or password.'};
       } else if (e.code == 'user-disabled') {
         return {
